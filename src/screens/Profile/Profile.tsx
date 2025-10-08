e056bd88ecfec7b0d7895deb2a2aaadb3e19f86e
srinivasansampath13 <sriofficial92@gmail.com>
2025-10-09 02:14:32 +0530

    1. Display email added. 2. Email verified check added. 3. Display picture added.

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,11 +1,11 @@
-import { View, Text, TouchableWithoutFeedback, Alert } from 'react-native'
+import { View, Text, TouchableWithoutFeedback, Alert, Image } from 'react-native'
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { AppDispatch } from '../../redux/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../../redux/store'
 import { logoutUser } from '../../redux/thunks/AuthThunks'
 
 const Profile = () => {
-
+  const { user } = useSelector((state: RootState) => state.auth)
   const dispatch = useDispatch<AppDispatch>();
 
   const logoutOnClick = async() => {
@@ -26,11 +26,41 @@ const Profile = () => {
     }
   }
 
+  const { email, picture, emailVerified } = user?.user
+  console.log('userInfo+++',user.user)
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Profile</Text>
+    <View style={{flex: 1}}>
+      <View style = {{justifyContent: 'center', alignItems: 'center', marginTop: '5%'}}>
+          {picture ? (
+            <Image 
+              source={{uri: picture}}
+              style = {{width: 100, height: 100, borderRadius: 50}}
+            />
+          ) : (
+            <View style={{width: 100, height: 100, borderRadius: 10, backgroundColor: 'gray', justifyContent: 'center', alignItems: 'center'}}>
+              <Text>No Image</Text>
+            </View>
+          )}
+        <View style = {{marginVertical: 8}}>
+            <Text style = {{fontSize: 14, fontWeight: '800'}}>{email}</Text>
+        </View>
+
+        <View style = {{marginVertical: 8}}>
+            {
+              emailVerified ? 
+              <View style = {{backgroundColor: 'green', paddingTop: 5, paddingBottom: 5, paddingHorizontal: 15, borderRadius: 25}}>
+                <Text style = {{color: 'white', fontSize: 15}}>{'Email Verified'}</Text>
+              </View> :
+              <View style = {{backgroundColor: 'red', paddingTop: 5, paddingBottom: 5, paddingHorizontal: 15, borderRadius: 25}}>
+                <Text style = {{color: 'white', fontSize: 15}}>{'Not Verified'}</Text>
+            </View>
+            }
+        </View>
+      </View>
       <TouchableWithoutFeedback onPress={() => logoutOnClick()}>
-        <View style = {{backgroundColor: 'blue', padding: 10, borderRadius: 5, marginTop: 20}}>
+        <View style = {{backgroundColor: 'blue', padding: 10, borderRadius: 5, marginTop: 20, width: '50%', justifyContent: 'center',
+          alignItems:'center', alignSelf:'center'
+        }}>
             <Text style = {{color: 'white'}}>Logout</Text>
         </View>
       </TouchableWithoutFeedback>
